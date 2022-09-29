b0dab120a2ff69246fe6d9fc8389e997e333d716
HBLocker <48737752+HBLocker@users.noreply.github.com>
2022-09-29 13:41:20 +0200

    Update sidh.py

--- sidh.py
+++ sidh.py
@@ -467,3 +467,42 @@ def Ladder3pt_dual(m, xP, xQ, xPQ, A, AliceorBob):
           VX, VZ, WX, WZ = xDBLADD(VX, VZ, WX, WZ, xPQ, A24)	
           
   return WX, WZ	
+
+    
+def secret_pt(x, y, m, AliceorBob):
+
+	
+	A24, C24 = 1, 2
+	X0, Z0, X1, Z1 = LADDER(-x, m, A24, C24, AliceorBob)
+	
+	RZ = (x * Z0)%p
+	RX0 = (X0 * x)%p
+	t4 = (X0 + RZ)%p
+	RZ = (X0 - RZ)%p
+	t0 = (t4**2)%p
+	RX0 = Z0 - RX0
+	t0 = (t0 * X1)%p
+	RX0 = (RX0 * RZ)%p
+	t2 = (y * Z1)%p
+	t1 = (y * Z0)%p
+	t2 = t2 + t2
+	RX1 = (t2 * Z0)%p
+	RX0 = (RX0 * Z1)%p
+	RX0 = RX0 - t0
+	t1 = (t1 * RX1)%p
+	t0 = (RX1**2)%p
+	t2 = (t2 * RX1)%p
+	RX1 = (t1 * RX0)%p
+	t3 = t1 + RX0
+	RX1 = RX1 + RX1
+	t1 = t1 - RX0
+	t1 = (t1 * t3)%p
+	RZ = (RZ**2)%p
+	t2 = (t2 * t4)%p
+	t2 = (t2 * RZ)%p
+	RZ = (t0 * RZ)%p
+	RX0 = t1 - t2
+	RX0 = RX0 % p
+	RX1 = RX1 % p
+	return RX0, RX1, RZ
+	
